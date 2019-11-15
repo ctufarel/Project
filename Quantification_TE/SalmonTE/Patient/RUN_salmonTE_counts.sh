551229d57cb24579b238df1fffc68539c6a5d7a0
rpg18 <49445224+rpg18@users.noreply.github.com>
2019-11-15 23:10:15 +0000

    Add files via upload

---
+++ Quantification_TE/SalmonTE/Patient/RUN_salmonTE_counts.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+### Script that runs SalmonTE quant, re-creates the sampleID vs Condition table, and runs SalmonTE test DE analysis 
+## SalmonTE requirements		
+path="/home/rpg18/Desktop/TFM/Server_alice2/Datasets/Fastq_files_pat/"				# input path where all paired-fastq files are stored
+path_out="/home/rpg18/Desktop/TFM/Pipelines/SalmonTE/Salmon_Patients_fastq/"		# output path for count matrix storing (SalmonTE quant funciton)
+path_out_test="/home/rpg18/Desktop/TFM/Pipelines/SalmonTE/Salmon_out_pat/"			# output path for DE analysis (SalmonTE test function)
+
+## creates new output path directories
+mkdir -pv $path_out
+mkdir -pv $path_out_test
+
+# for each fastq file stored in $path
+for i in $path*
+do
+	total=$total" $i" # $total stores all fastq files
+done
+
+## run TE quantification with SalmonTE quant
+python3 SalmonTE.py quant --reference=hs --exprtype=count --num_threads=4 --outpath="$path_out" $total
+
+## re-write updated condition.csv file
+python3 condition_file.py $path_out
+
+## run DE analysis using SalmonTE test function
+var1=$path_out
+var2=$path_out_test
+
+sh test_salmon.sh $var1 $var2
