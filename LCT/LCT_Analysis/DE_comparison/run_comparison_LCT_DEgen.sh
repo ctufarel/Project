9bac7b97b4b49dcf3ab9fca290dfecb248c0c53f
rpg18 <49445224+rpg18@users.noreply.github.com>
2019-11-19 01:32:31 +0000

    Create run_comparison_LCT_DEgen.sh

---
+++ LCT/LCT_Analysis/DE_comparison/run_comparison_LCT_DEgen.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+''' Script-4 that extracts gene names from annotated shared-Infl-Tumour-LCTs if there are differentially expressed in both patients (normal vs tumour) and Inflammation (normal vs inflamed) datasets '''
+echo \n
+
+outpath=$@
+# add prefix 'infl' for inflammation and 'pat' for patient dataset
+var1='./TEtrans_out/infl_DEfold2_gene_TE.txt' 			# path to DEGenes for inflammation 
+var2='./TEtrans_out/pat_sample_4_sigdiff_gene_TE.txt'	# path to DEGenes for patient dataset
+
+# generates finalgenes_func.txt file
+python3 geneDE2geneLCT.py $outpath $var1 $var2
+# generates finalgenes_pat_func.txt file
+python3 geneName2_pos_pat.py $outpath
+# generates finalgenes_compared_func.txt
+python3 infcomparison.py $outpath $var1 $var2
