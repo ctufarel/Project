49126f70ae8a0d95b9863cbaefde52f7addf580d
rpg18 <49445224+rpg18@users.noreply.github.com>
2019-11-21 18:23:10 +0000

    Update run_test_salmon.sh

--- Quantification_TE/SalmonTE/Patient/run_test_salmon.sh
+++ Quantification_TE/SalmonTE/Patient/run_test_salmon.sh
@@ -1,3 +1,3 @@
 # Script that performs the DE using SalmonTE test parameter:
 
-python3 SalmonTE.py test --inpath=$1 --outpath=$2 --tabletype=csv --figtype=jpg --analysis_type=DE --conditions=control,treatment
+python3 SalmonTE.py test --inpath=$1 --outpath=$2 --tabletype=csv --figtype=jpg --analysis_type=DE --conditions=$3,$4
